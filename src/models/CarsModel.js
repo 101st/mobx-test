c9108211fb5696ec3fe6daf8525a058a1ec3ef0f
Latexovich <discord.latex@gmail.com>
2019-11-02 18:21:03 +0300

    add loader refactoring

--- src/models/CarsModel.js
+++ src/models/CarsModel.js
@@ -2,10 +2,11 @@ import { observable, computed, action } from 'mobx';
 import * as api from '../apis/cars';
 
 export default class CarsModel {
-  @observable cars;
-  @observable headers;
-  @observable page;
-  @observable per_page;
+  @observable cars = [];
+  @observable headers = {};
+  @observable page = 1;
+  @observable per_page = 15;
+  @observable loading = true;
 
   @computed
   get carsCount() {
@@ -20,18 +21,13 @@ export default class CarsModel {
   @action.bound
   async fetchCars(page = 1, per_page = 15) {
     try {
+      this.loading = true;
       let { data, headers } = await api.getCars(page, per_page);
       this.headers = headers;
       this.cars = data.map(car => { car.key = car.id; return car; });
+      this.loading = false;
     } catch (err) {
       throw err;
     }
-  }
-
-  constructor() {
-    this.page = 1;
-    this.per_page = 15;
-    this.cars = [];
-    this.headers = {};
   }
 }
